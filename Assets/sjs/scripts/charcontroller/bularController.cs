df09ab6df21c94bb4f2de79456dfe3ee275139ef
Simonjs <simonjs@live.dk>
2022-03-25 14:55:04 +0100

    wip on creating new char controller
    in air movement does not function

--- Assets/sjs/scripts/charcontroller/bularController.cs
+++ Assets/sjs/scripts/charcontroller/bularController.cs
@@ -9,11 +9,8 @@ public class bularController : MonoBehaviour
     private Rigidbody2D RB;
     public float jumpForce;
     private Vector3 playerScale;
-    private bool canMove = true;
-    //slide
-    public float slideMultiplier = 5;
-    public float slideCD = 1;
-    private float slideTimer;
+    
+    
     
     private void Start()
     {
@@ -23,21 +20,20 @@ public class bularController : MonoBehaviour
 
     private void Update()
     {
-        basemovement();
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            slide();
-        }
-
-        
+        basemovement();   
     }
 
+private void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
 
-
-
-
-
+        if (moveInput != 0)
+        {
+            playerScale.x = Mathf.Abs(playerScale.x)*Mathf.Sign(moveInput);
+            transform.localScale = playerScale;
+        }
+        
+    }
 
     private void basemovement()
     {
@@ -46,45 +42,16 @@ public class bularController : MonoBehaviour
             RB.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
 
-        if (moveInput < 0)
+        if(RB.velocity.y == 0)
         {
-            
-            playerScale.x = Mathf.Abs(playerScale.x);
-            transform.localScale = playerScale;
+            transform.Translate(moveInput * speed * Time.deltaTime, 0, 0);
         }
-        else if (moveInput > 0)
+        else
         {
-            
-            playerScale.x = Mathf.Abs(playerScale.x)-1;
-            transform.localScale = playerScale;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        moveInput = Input.GetAxis("Horizontal");
-
-        if (canMove)
-        {
-            
-            transform.Translate(moveInput * speed*Time.deltaTime, 0,0);
-        } else if (slideTimer<Time.time)
-        {
-            canMove = true;
-        }
-
-    }
-
-
-    private void slide()
-    {
-        if (canMove)
-        {
-            canMove = false;
-            RB.AddForce(new Vector2(speed * slideMultiplier, 0) * playerScale * -1, ForceMode2D.Impulse);
-            slideTimer = Time.time + slideCD;
+            //RB.AddForce(new Vector2(moveInput * speed*Time.deltaTime, 0), ForceMode2D.Impulse);
         }
     }
 
     
+
 }
