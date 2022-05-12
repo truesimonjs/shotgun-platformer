b7350b3055140c8b2cef226dcb07dfbaef2c043d
Simonjs <simonjs@live.dk>
2022-05-12 10:15:09 +0200

    gave smallshroom a trigger

--- Assets/sjs/scripts/menuScript.cs
+++ Assets/sjs/scripts/menuScript.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class menuScript : MonoBehaviour
 {
     public GameObject startMenu;
+    public GameObject levelSelect;
     public GameObject levelselect;
+    public GameObject button;
     public List<string> levels;
     public void PressStart()
     {
         startMenu.SetActive(false);
+        levelselect.SetActive(true);
     }
     public void PressOptions()
     {
@@ -22,12 +26,12 @@ public class menuScript : MonoBehaviour
 
     public void SelectLevel(int level)
     {
-
+        SceneManager.LoadScene(levels[level]);
     }
 
     private void Start()
     {
-       
+        Instantiate(button);
     }
 
 }
